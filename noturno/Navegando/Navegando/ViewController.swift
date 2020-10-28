ab08e446d47c5220d67a9191bd04afa124162e2c
sergiolmm <sergio@unicamp.br>
2020-10-28 19:51:07 -0400

    telas nav

--- noturno/Navegando/Navegando/ViewController.swift
+++ noturno/Navegando/Navegando/ViewController.swift
@@ -12,8 +12,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationController?.setNavigationBarHidden(true, animated: true)
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
     @IBAction func unwindViewWithSegue(segue  : UIStoryboardSegue){
         
     }
